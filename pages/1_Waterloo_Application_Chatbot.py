b0199ada8611df75fc88dbdde4c3f900c16d560b
TAO (DAVID) SHAN <shan2000814@gmail.com>
2024-04-02 17:42:08 -0400

    Update 1_Waterloo_Application_Chatbot.py

--- pages/1_Waterloo_Application_Chatbot.py
+++ pages/1_Waterloo_Application_Chatbot.py
@@ -4,9 +4,9 @@ import os
 import nltk
 nltk.download('punkt')
 
-# import pysqlite3
-# import sys
-# sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+import pysqlite3
+import sys
+sys.modules['sqlite3'] = pysqlite3
 
 # initial Setting
 max_output_token = 4096
